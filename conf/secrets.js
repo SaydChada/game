7fdb1459e3e4fdfdae9bcad27c680167438efebd
CHADA Saïd <said@n-sens.com>
2017-03-19 21:25:13 +0100

    muste be in gitignore

--- conf/secrets.js
+++
@@ -1,8 +0,0 @@
-/**
- * Define salt / hashphrase for modules
- * @type {{cookie: string, session: string}}
- */
-module.exports = {
-    cookie          : "regr5e9g4-re9g8",
-    session         : "'rez98f79r87-2'"
-};
