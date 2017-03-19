cd68a8d44926084a0e6fd3648bcac95c71d048d4
Chada <said@n-sens.com>
2017-03-19 13:46:14 +0100

    added template game lobby

--- app/public/js/contact.js
+++ app/public/js/contact.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
     var $characterLeft = $('#characterLeft');
-    $characterLeft.text('140 characters left');
+    $characterLeft.text('200 characters réstants');
     $('#message').keydown(function () {
         var max = 200;
         var len = $(this).val().length;
         if (len >= max) {
-            $characterLeft.text('You have reached the limit');
-            $characterLeft.addClass('red');
+            $characterLeft.text('Limite de caractères atteinte');
+            $characterLeft.addClass('text-danger');
             $('#btnSubmit').addClass('disabled');
         }
         else {
             var ch = max - len;
-            $characterLeft.text(ch + ' characters left');
+            $characterLeft.text(ch + ' caractères restants');
             $('#btnSubmit').removeClass('disabled');
-            $characterLeft.removeClass('red');
+            $characterLeft.removeClass('text-danger');
         }
     });
 });
